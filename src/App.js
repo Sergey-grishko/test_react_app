19b8817348938073242d2f9f3c628ca548ac1caf
Sergey <megusta970@gmail.com>
2019-02-04 11:28:33 +0200

    updata

--- src/App.js
+++ src/App.js
@@ -1,28 +1,29 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import arrow from './arrow-down.svg'
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <div className="Container">
+                    <span className="Title">Заголовок</span>
+                    <div className="line"/>
+                    <img src={arrow} className='arrow'/>
+                    <select className="select">
+                        <option>Селект</option>
+                    </select>
+                    <div className="subtitle">Строчный текст</div>
+                    <div className="rowInput">
+                        <input placeholder="Значение" className="input"/>
+                        <div className="hyphen">-</div>
+                        <input placeholder="Второе" className="input"/>
+                    </div>
+                    <button className="button">Кнопка</button>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
